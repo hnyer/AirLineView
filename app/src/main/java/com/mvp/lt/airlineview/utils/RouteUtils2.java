06b0dae6e204a792963b5f8707b2691e92ff4278
刘涛 <liutao@163.com>
2018-08-13 16:41:45 +0800

    算法

--- app/src/main/java/com/mvp/lt/airlineview/utils/RouteUtils2.java
+++ app/src/main/java/com/mvp/lt/airlineview/utils/RouteUtils2.java
@@ -483,4 +483,18 @@ public class RouteUtils2 {
 
         return yaw;
     }
+
+    /**
+     * 根据一个点的经纬度 ，方位角和距离得到另外一个点的经纬度
+     *
+     * @param distance  KM
+     * @param latlngA
+     * @param angle：角度
+     * @return
+     */
+    public static LatLng getLatlng(float distance, LatLng latlngA, double angle) {
+        return new LatLng(latlngA.latitude + (distance * Math.cos(angle * Math.PI / 180)) / 111,
+                latlngA.longitude + (distance * Math.sin(angle * Math.PI / 180)) / (111 * Math.cos(latlngA.latitude * Math.PI / 180))
+        );
+    }
 }
